fa0ed1ff44a6602ed4d51035b3f81189534d7c6a
tiago-kelis <tiagokelis2017@gmail.com>
2025-07-13 23:13:09 -0300

    finalisando a parte de requisições http

--- gam3r.store/apps/backend/src/pedido/pedido.module.ts
+++ gam3r.store/apps/backend/src/pedido/pedido.module.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { Module} from '@nestjs/common';
 import { PedidoController } from './pedido.controller';
 import { PedidoPrisma } from './pedido.prisma';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [PedidoController],
   providers: [PedidoPrisma],
 })

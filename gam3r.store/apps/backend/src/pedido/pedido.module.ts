66e1b3fed5a82716e10010bdcc91b7df10a854bf
tiago-kelis <tiagokelis2017@gmail.com>
2025-07-13 22:42:53 -0300

    criando o pedido.prisma um provider

--- gam3r.store/apps/backend/src/pedido/pedido.module.ts
+++ gam3r.store/apps/backend/src/pedido/pedido.module.ts
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { PedidoController } from './pedido.controller';
+import { PedidoPrisma } from './pedido.prisma';
 
 @Module({
-  controllers: [PedidoController]
+  controllers: [PedidoController],
+  providers: [PedidoPrisma],
 })
 export class PedidoModule {}
